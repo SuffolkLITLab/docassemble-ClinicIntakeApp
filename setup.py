b670c152d6f64366d2fad2ee9c8a2e674eff7b1f
Bryce Willey <bryce.willey@suffolk.edu>
2025-11-24 16:58:48 -0500

    Use implicit namespaces over explicit namespaces

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 from fnmatch import fnmatchcase
 from distutils.util import convert_path
 
@@ -51,8 +51,7 @@ setup(name='docassemble.ClinicIntakeApp',
       author_email='mcarroll11@su.suffolk.edu',
       license='The MIT License (MIT)',
       url='https://github.com/SuffolkLITLab/docassemble-ClinicIntakeFax',
-      packages=find_packages(),
-      namespace_packages=['docassemble'],
+      packages=find_namespace_packages(),
       install_requires=['docassemble.MAVirtualCourt>=1.0.20'],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/ClinicIntakeApp/', package='docassemble.ClinicIntakeApp'),
